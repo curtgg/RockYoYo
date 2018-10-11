af4430a34f5bf969dac3a08327b3faf4ad030d5d
curtgg <cgoud@ualberta.ca>
2018-10-10 21:22:49 -0600

    Player movement and failed rope attempt

---
+++ RockYoYo/Assets/Scripts/RopeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeController : MonoBehaviour {
+
+    bool isRoot = false;
+    bool isTail = false;
+
+    public GameObject prefab;
+	// Use this for initialization
+	void Start () {
+        if (GameObject.FindGameObjectsWithTag("Rope").Length == 1)
+        {
+            this.isRoot = true;
+            //GetComponent<Renderer>().bounds
+            //Instantiate(prefab, );
+        }	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		this.transform
+	}
+}
